fb8a24317730a77cd70246f48876ed994a743f81
genndy007 <kochevgenazp@gmail.com>
2020-12-14 00:08:50 +0200

    Now game draws a dice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 from button import Button
 from const import POINTS_TO_WIN
 from const import TURN_HUMAN, TURN_BOT
@@ -51,21 +52,37 @@ def print_bot_stats(bot_total, bot_turn_score, screen, font):
         offset += FONT_SIZE
 
 
+# load images of dice
+def load_dice_images(path):
+    dct = dict()
+    for i in range(1, 7):
+        img = pygame.image.load(f'{path}dice{i}.png').convert()
+
+        dct[i] = img
+    
+    return dct
+
 
 
 
 # main game loop
 def game():
+    # Pre-loading of resource
     status_font = pygame.font.Font('resources/font/consola.ttf', FONT_SIZE)
+    dice_imgs = load_dice_images('resources/img/')
+
     # Gaming pre-settings
     human_total, bot_total = 0, 0
     human_turn_score, bot_turn_score = 0, 0 
     turn = TURN_HUMAN
+    current_dice = 1
 
-    running = True
     # Buttons initialize
     button_roll = Button(300, 400, 200, 50, color=RED, text='Roll')
     button_hold = Button(300, 500, 200, 50, color=BLUE, text='Hold')
+    
+    # Main cycle control
+    running = True
     while running:
         # Fill the screen
         screen.fill(WHITE)
@@ -75,7 +92,8 @@ def game():
         # Show statuses of human and bot
         print_human_stats(human_total, human_turn_score, screen, status_font)
         print_bot_stats(bot_total, bot_turn_score, screen, status_font)
-
+        # Show current dice as a picture
+        screen.blit(dice_imgs[current_dice], (350, 100))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -98,6 +116,8 @@ def game():
 
         clock.tick(FPS)
         pygame.display.flip()
+
+
 
 
 
