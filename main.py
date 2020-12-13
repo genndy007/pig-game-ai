1429dbb4b3e2e5446d9590f627b52c9b8df3eed4
genndy007 <kochevgenazp@gmail.com>
2020-12-13 23:29:38 +0200

    Added showing of bot and human results

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame
+
 from button import Button
 from const import POINTS_TO_WIN
+from const import TURN_HUMAN, TURN_BOT
+from const import FONT_SIZE
+
+
 # Screen params
 WIN_WIDTH = 800
 WIN_HEIGHT = 600
@@ -23,10 +28,40 @@ pygame.display.set_caption('Pig Dice Game')
 clock = pygame.time.Clock()
 
 
+# Show human stats at game screen
+def print_human_stats(human_total, human_turn_score, screen, font):
+    text = f'HUMAN\nTotal score: {human_total}\nTurn score: {human_turn_score}'
+    offset = 0
+
+    for line in text.split('\n'):
+        rendered = font.render(line, 1, BLACK)
+        screen.blit(rendered, (50, 100+4*offset))
+        offset += FONT_SIZE
+
+
+
+# Show bot stats at game screen
+def print_bot_stats(bot_total, bot_turn_score, screen, font):
+    text = f'BOT\nTotal score: {bot_total}\nTurn score: {bot_turn_score}'
+    offset = 0
+
+    for line in text.split('\n'):
+        rendered = font.render(line, 1, BLACK)
+        screen.blit(rendered, (600, 100+4*offset))
+        offset += FONT_SIZE
+
+
+
+
+
 # main game loop
 def game():
+    status_font = pygame.font.Font('resources/font/consola.ttf', FONT_SIZE)
     # Gaming pre-settings
     human_total, bot_total = 0, 0
+    human_turn_score, bot_turn_score = 0, 0 
+    turn = TURN_HUMAN
+
     running = True
     # Buttons initialize
     button_roll = Button(300, 400, 200, 50, color=RED, text='Roll')
@@ -37,7 +72,10 @@ def game():
         # Draw buttons
         button_roll.draw(screen)
         button_hold.draw(screen)
-        
+        # Show statuses of human and bot
+        print_human_stats(human_total, human_turn_score, screen, status_font)
+        print_bot_stats(bot_total, bot_turn_score, screen, status_font)
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -45,12 +83,18 @@ def game():
 
             if event.type == pygame.MOUSEBUTTONDOWN:
 
-                if button_roll.isOver(event.pos):
-                    pass # do a barrel roll
+                if turn == TURN_HUMAN:   # if it's a human to roll then reasonable to check pressing buttons
                 
-                if button_hold.isOver(event.pos):
-                    pass # hold back
+                    if button_roll.isOver(event.pos):
+                        pass # do a barrel roll
+                    
+                    if button_hold.isOver(event.pos):
+                        pass # hold back
+                        turn = TURN_BOT  # give turn to bot if end of move
 
+        # After every check of human action, it's time for bot actions
+        if turn == TURN_BOT:
+            pass # bot does things
 
         clock.tick(FPS)
         pygame.display.flip()
